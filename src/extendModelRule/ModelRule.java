fb7a8f2d498d7e92ca5fbf7ace4768bc2464d0dd
masanori-10 <s.masanori.vx@gmail.com>
2017-02-20 22:36:06 +0900

    fixed sensor

--- src/extendModelRule/ModelRule.java
+++ src/extendModelRule/ModelRule.java
@@ -38,7 +38,8 @@ public class ModelRule {
 	private SteeringCar car;
 	private List<MapObject> crossingPointList;
 	private MapObject[] route;
-	private String[] routeName = { "01", "02", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "22" };
+	private String[] routeName = { "01", "02", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14",
+			"15", "22" };
 	private int currentRoute = 0;
 	private int RouteSize;
 
@@ -100,6 +101,7 @@ public class ModelRule {
 		}
 	}
 
+	/* 自車中心のセンサー(現在未使用) */
 	private double[] getSensorPoint(int index) {
 		double x = car.getPosition().getX();
 		double z = car.getPosition().getZ();
@@ -138,10 +140,101 @@ public class ModelRule {
 		return pair;
 	}
 
+	private boolean findTarget(Vector3f target, int[] pointIndex) {
+		double dx[] = new double[5];
+		double dz[] = new double[5];
+		for (int i = 0; i < 4; i++) {
+			double[] pair = getSensorPoint(pointIndex[i]);
+			dx[i] = pair[0] - target.getX();
+			dz[i] = pair[1] - target.getZ();
+		}
+		dx[4] = dx[0];
+		dz[4] = dz[0];
+		double angle = 0;
+		for (int i = 0; i < 4; i++) {
+			angle += Math.atan2(dx[i] * dz[i + 1] - dz[i] * dx[i + 1], dx[i] * dx[i + 1] + dz[i] * dz[i + 1]);
+		}
+		if (2 * Math.PI <= angle + 0.00001 && 2 * Math.PI >= angle - 0.00001) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean precedingCarSensor(boolean inEmergency) {
+		int[] sensorPoints = { inEmergency ? 6 : 1, inEmergency ? 8 : 3, 13, 11 };
+		for (TrafficObject car : PhysicalTraffic.getTrafficObjectList()) {
+			if (car.getName().contains("car")) {
+				if (findTarget(car.getPosition(), sensorPoints)) {
+					if (ondebugFlame) {
+						System.out.println(inEmergency ? "find preceding car in emergency" : "find preceding car");
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean pedestrianSensor(boolean inEmergency) {
+		int[] sensorPoints = { inEmergency ? 6 : 0, inEmergency ? 8 : 4, inEmergency ? 13 : 14, inEmergency ? 11 : 10 };
+		for (TrafficObject pedestrian : PhysicalTraffic.getTrafficObjectList()) {
+			if (pedestrian.getName().contains("pedestrian")) {
+				if (findTarget(pedestrian.getPosition(), sensorPoints)) {
+					if (ondebugFlame) {
+						System.out.println(inEmergency ? "find predestrian in emergency" : "find predestrian");
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean trafficLightSensor(boolean inEmergency) {
+		int[] sensorPoints = { inEmergency ? 5 : 0, inEmergency ? 9 : 4, 14, 10 };
+		for (TrafficLight trafficLight : sim.getTrafficLightCenter().getGrobalTrafficLightList()) {
+			if (findTarget(trafficLight.getLocalPosition(), sensorPoints)) {
+				double direction = Math.toDegrees(trafficLight.getRotation().toAngleAxis(new Vector3f(0, 1, 0)));
+				direction = trafficLight.getRotation().getY() > 0 ? direction : -direction;
+				double carDirection = getCarDirection();
+				if ((direction < carDirection + 5 && direction > carDirection - 5) || direction < carDirection - 355
+						|| direction > carDirection + 355) {
+					if (trafficLight.getState() != TrafficLightState.GREEN) {
+						if (ondebugFlame) {
+							System.out.println(inEmergency ? "find traffic light in emergency" : "find traffic light");
+						}
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean crossingPointSensor(boolean inEmergency) {
+		int[] sensorPoints = { inEmergency ? 6 : 1, inEmergency ? 8 : 3, 18, 16 };
+		for (MapObject crossingPoint : crossingPointList) {
+			if (findTarget(crossingPoint.getLocation(), sensorPoints)) {
+				if (ondebugFlame) {
+					System.out.println("find crossing point[" + crossingPoint.getName() + "]");
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	/* */
+
+	/* 先読み型センサー */
 	private SensorPointSet getSensorPoint2(double left, double right, double forward, double behind) {
-		double alpha = Math.toRadians(getCarDirection()) + steeringValue * 10;
+		double alpha = Math.toRadians(getCarDirection());
 		double pivotX = car.getPosition().getX() - Math.sin(alpha) * car.getCurrentSpeedKmh() / 3;
 		double pivotZ = car.getPosition().getZ() - Math.cos(alpha) * car.getCurrentSpeedKmh() / 3;
+		if(steeringValue > 0){
+			behind += steeringValue * 30;
+		} else if (steeringValue < 0) {
+			behind -= steeringValue * 30;
+		}
 		SensorPointSet sps = new SensorPointSet();
 		sps.addPoint(pivotX - Math.sin(alpha) * forward - Math.sin(alpha + Math.PI / 2) * left,
 				pivotZ - Math.cos(alpha) * forward - Math.cos(alpha + Math.PI / 2) * left);
@@ -189,27 +282,8 @@ public class ModelRule {
 		}
 	}
 
-	private boolean findTarget(Vector3f target, int[] pointIndex) {
-		double dx[] = new double[5];
-		double dz[] = new double[5];
-		for (int i = 0; i < 4; i++) {
-			double[] pair = getSensorPoint(pointIndex[i]);
-			dx[i] = pair[0] - target.getX();
-			dz[i] = pair[1] - target.getZ();
-		}
-		dx[4] = dx[0];
-		dz[4] = dz[0];
-		double angle = 0;
-		for (int i = 0; i < 4; i++) {
-			angle += Math.atan2(dx[i] * dz[i + 1] - dz[i] * dx[i + 1], dx[i] * dx[i + 1] + dz[i] * dz[i + 1]);
-		}
-		if (2 * Math.PI <= angle + 0.00001 && 2 * Math.PI >= angle - 0.00001) {
-			return true;
-		}
-		return false;
-	}
-
-	private boolean findTarget2(Vector3f target, double left, double right, double forward, double behind) {
+	private boolean findTarget2(Vector3f target, double left, double right, double forward, double behind,
+			boolean ondebug) {
 		SensorPointSet sps = getSensorPoint2(left, right, forward, behind);
 		double angle = 0;
 		double dx[] = sps.getdx(target);
@@ -218,22 +292,13 @@ public class ModelRule {
 			angle += Math.atan2(dx[i] * dz[i + 1] - dz[i] * dx[i + 1], dx[i] * dx[i + 1] + dz[i] * dz[i + 1]);
 		}
 		if (2 * Math.PI <= angle + 0.00001 && 2 * Math.PI >= angle - 0.00001) {
-			return true;
-		}
-		return false;
-	}
-
-	private boolean precedingCarSensor(boolean inEmergency) {
-		int[] sensorPoints = { inEmergency ? 6 : 1, inEmergency ? 8 : 3, 13, 11 };
-		for (TrafficObject car : PhysicalTraffic.getTrafficObjectList()) {
-			if (car.getName().contains("car")) {
-				if (findTarget(car.getPosition(), sensorPoints)) {
-					if (ondebugFlame) {
-						System.out.println(inEmergency ? "find preceding car in emergency" : "find preceding car");
-					}
-					return true;
+			for (int i = 0; i < 4; i++) {
+				if (ondebug) {
+					System.out.println("sensorX[" + i + "]" + sps.x[i]);
+					System.out.println("sensorZ[" + i + "]" + sps.z[i]);
 				}
 			}
+			return true;
 		}
 		return false;
 	}
@@ -245,7 +310,7 @@ public class ModelRule {
 		float behind = 0;
 		for (TrafficObject car : PhysicalTraffic.getTrafficObjectList()) {
 			if (car.getName().contains("car")) {
-				if (findTarget2(car.getPosition(), left, right, forward, behind)) {
+				if (findTarget2(car.getPosition(), left, right, forward, behind, ondebugFlame)) {
 					if (ondebugFlame) {
 						System.out.println(inEmergency ? "find preceding car in emergency" : "find preceding car");
 					}
@@ -256,29 +321,16 @@ public class ModelRule {
 		return false;
 	}
 
-	private boolean pedestrianSensor(boolean inEmergency) {
-		int[] sensorPoints = { inEmergency ? 6 : 0, inEmergency ? 8 : 4, inEmergency ? 13 : 14, inEmergency ? 11 : 10 };
-		for (TrafficObject pedestrian : PhysicalTraffic.getTrafficObjectList()) {
-			if (pedestrian.getName().contains("pedestrian")) {
-				if (findTarget(pedestrian.getPosition(), sensorPoints)) {
-					if (ondebugFlame) {
-						System.out.println(inEmergency ? "find predestrian in emergency" : "find predestrian");
-					}
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+
 
 	private boolean pedestrianSensor2(boolean inEmergency) {
-		float left = SensorRangeCarWidth;
+		float left = SensorRangeSide;
 		float right = SensorRangeCarWidth;
 		float forward = inEmergency ? SensorRangeEmergency : SensorRangeForward;
 		float behind = 0;
 		for (TrafficObject pedestrian : PhysicalTraffic.getTrafficObjectList()) {
 			if (pedestrian.getName().contains("pedestrian")) {
-				if (findTarget2(pedestrian.getPosition(), left, right, forward, behind)) {
+				if (findTarget2(pedestrian.getPosition(), left, right, forward, behind, ondebugFlame)) {
 					if (ondebugFlame) {
 						System.out.println(inEmergency ? "find predestrian in emergency" : "find predestrian");
 					}
@@ -289,26 +341,7 @@ public class ModelRule {
 		return false;
 	}
 
-	private boolean trafficLightSensor(boolean inEmergency) {
-		int[] sensorPoints = { inEmergency ? 5 : 0, inEmergency ? 9 : 4, 14, 10 };
-		for (TrafficLight trafficLight : sim.getTrafficLightCenter().getGrobalTrafficLightList()) {
-			if (findTarget(trafficLight.getLocalPosition(), sensorPoints)) {
-				double direction = Math.toDegrees(trafficLight.getRotation().toAngleAxis(new Vector3f(0, 1, 0)));
-				direction = trafficLight.getRotation().getY() > 0 ? direction : -direction;
-				double carDirection = getCarDirection();
-				if ((direction < carDirection + 5 && direction > carDirection - 5) || direction < carDirection - 355
-						|| direction > carDirection + 355) {
-					if (trafficLight.getState() != TrafficLightState.GREEN) {
-						if (ondebugFlame) {
-							System.out.println(inEmergency ? "find traffic light in emergency" : "find traffic light");
-						}
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
+
 
 	private boolean trafficLightSensor2(boolean inEmergency) {
 		float left = SensorRangeSide;
@@ -316,7 +349,7 @@ public class ModelRule {
 		float forward = inEmergency ? SensorRangeEmergency : SensorRangeForward;
 		float behind = 0;
 		for (TrafficLight trafficLight : sim.getTrafficLightCenter().getGrobalTrafficLightList()) {
-			if (findTarget2(trafficLight.getLocalPosition(), left, right, forward, behind)) {
+			if (findTarget2(trafficLight.getLocalPosition(), left, right, forward, behind, ondebugFlame)) {
 				double direction = Math.toDegrees(trafficLight.getRotation().toAngleAxis(new Vector3f(0, 1, 0)));
 				direction = trafficLight.getRotation().getY() > 0 ? direction : -direction;
 				double carDirection = getCarDirection();
@@ -333,26 +366,13 @@ public class ModelRule {
 		return false;
 	}
 
-	private boolean crossingPointSensor(boolean inEmergency) {
-		int[] sensorPoints = { inEmergency ? 6 : 1, inEmergency ? 8 : 3, 18, 16 };
-		for (MapObject crossingPoint : crossingPointList) {
-			if (findTarget(crossingPoint.getLocation(), sensorPoints)) {
-				if (ondebugFlame) {
-					System.out.println("find crossing point[" + crossingPoint.getName() + "]");
-				}
-				return true;
-			}
-		}
-		return false;
-	}
-
 	private boolean crossingPointSensor2(boolean inEmergency) {
 		float left = SensorRangeCarWidth;
 		float right = SensorRangeCarWidth;
 		float forward = inEmergency ? SensorRangeEmergency : SensorRangeForward;
 		float behind = 0;
 		for (MapObject crossingPoint : crossingPointList) {
-			if (findTarget2(crossingPoint.getLocation(), left, right, forward, behind)) {
+			if (findTarget2(crossingPoint.getLocation(), left, right, forward, behind, false)) {
 				if (ondebugFlame) {
 					System.out.println("find crossing point[" + crossingPoint.getName() + "]");
 				}
@@ -377,70 +397,9 @@ public class ModelRule {
 	private boolean hittingNaviPoint(Vector3f target) {
 		return this.car.getPosition().distance(target) < HittingDistance;
 	}
+	/* */
 
-	// private boolean findTargetRight1(Vector3f target) {
-	// double targetDirection = getTargetDirection(target);
-	// double carDirection = getCarDirection();
-	// return (targetDirection < carDirection - 1 || targetDirection >
-	// carDirection + 181)
-	// && targetDirection > carDirection - 179 && targetDirection < carDirection
-	// + 359;
-	// }
-	//
-	// private boolean findTargetRight2(Vector3f target) {
-	// double targetDirection = getTargetDirection(target);
-	// double carDirection = getCarDirection();
-	// return (targetDirection < carDirection - 5 || targetDirection >
-	// carDirection + 185)
-	// && targetDirection > carDirection - 175 && targetDirection < carDirection
-	// + 355;
-	// }
-	//
-	// private boolean findTargetRight3(Vector3f target) {
-	// double targetDirection = getTargetDirection(target);
-	// double carDirection = getCarDirection();
-	// return (targetDirection < carDirection - 20 || targetDirection >
-	// carDirection + 200)
-	// && targetDirection > carDirection - 160 && targetDirection < carDirection
-	// + 340;
-	// }
-	//
-	// private boolean findTargetLeft1(Vector3f target) {
-	// double targetDirection = getTargetDirection(target);
-	// double carDirection = getCarDirection();
-	// return (targetDirection > carDirection + 1 || targetDirection <
-	// carDirection - 181)
-	// && targetDirection < carDirection + 179 && targetDirection > carDirection
-	// - 359;
-	// }
-	//
-	// private boolean findTargetLeft2(Vector3f target) {
-	// double targetDirection = getTargetDirection(target);
-	// double carDirection = getCarDirection();
-	// return (targetDirection > carDirection + 5 || targetDirection <
-	// carDirection - 185)
-	// && targetDirection < carDirection + 175 && targetDirection > carDirection
-	// - 355;
-	// }
-	//
-	// private boolean findTargetLeft3(Vector3f target) {
-	// double targetDirection = getTargetDirection(target);
-	// double carDirection = getCarDirection();
-	// return (targetDirection > carDirection + 20 || targetDirection <
-	// carDirection - 200)
-	// && targetDirection < carDirection + 160 && targetDirection > carDirection
-	// - 340;
-	// }
-
-	private double getSteeringValue(Vector3f target) {
-		double handLink = getTargetDirection(target) - getCarDirection();
-		if (handLink < 1 && handLink > -1)
-			return 0;
-		handLink = handLink > 180 ? handLink - 360 : handLink;
-		handLink = handLink < -180 ? 360 + handLink : handLink;
-		return (handLink > 0 ? Math.log10(handLink) : -Math.log10(-handLink)) / 4;
-	}
-
+	/* 実行ルール */
 	private void emergencyRule() {
 		objectiveSpeed = 0;
 	}
@@ -462,8 +421,8 @@ public class ModelRule {
 	}
 
 	private void hittingNaviPointRule(MapObject hittingPoint) {
-		objectiveSpeed = objectiveSpeed > CurveSpeed ? CurveSpeed : objectiveSpeed;
 		if (hittingPoint == route[currentRoute]) {
+			objectiveSpeed = objectiveSpeed > CurveSpeed ? CurveSpeed : objectiveSpeed;
 			currentRoute++;
 			if (currentRoute == RouteSize) {
 				if (rotation) {
@@ -474,8 +433,13 @@ public class ModelRule {
 				}
 			}
 		}
+		if (currentRoute > 0 && hittingPoint == route[currentRoute - 1]) {
+			objectiveSpeed = objectiveSpeed > CurveSpeed ? CurveSpeed : objectiveSpeed;
+		}
 	}
+	/* */
 
+	/* 自動操作 */
 	private double getTargetDirection(Vector3f target) {
 		return Math.toDegrees(Math.atan2(this.car.getPosition().getX() - target.getX(),
 				this.car.getPosition().getZ() - target.getZ()));
@@ -484,6 +448,16 @@ public class ModelRule {
 	private double getCarDirection() {
 		double direction = Math.toDegrees(car.getRotation().toAngleAxis(new Vector3f(0, 1, 0)));
 		return car.getRotation().getY() > 0 ? direction : -direction;
+	}
+
+	private double getSteeringValue(Vector3f target) {
+		double handLink = getTargetDirection(target) - getCarDirection();
+		if (handLink < 1 && handLink > -1)
+			return 0;
+		handLink = handLink > 180 ? handLink - 360 : handLink;
+		handLink = handLink < -180 ? 360 + handLink : handLink;
+		handLink = (handLink > 0 ? Math.log10(handLink) : -Math.log10(-handLink)) / 4;
+		return handLink;
 	}
 
 	private void setSpeed(float objectiveSpeed) {
@@ -506,25 +480,11 @@ public class ModelRule {
 		if (ondebugFlame) {
 			System.out.println("current steering value = " + steeringValue);
 		}
-		// if (findTargetLeft3(target)) {
-		// steeringValue = -.3f;
-		// } else if (findTargetLeft2(target)) {
-		// steeringValue = -.2f;
-		// } else if (findTargetLeft1(target)) {
-		// steeringValue = -.1f;
-		// } else if (findTargetRight3(target)) {
-		// steeringValue = .3f;
-		// } else if (findTargetRight2(target)) {
-		// steeringValue = .2f;
-		// } else if (findTargetRight1(target)) {
-		// steeringValue = .1f;
-		// } else {
-		// steeringValue = 0;
-		// }
 		CANClient canClient = Simulator.getCanClient();
 		if (canClient != null)
 			canClient.suppressSteering();
 		sim.getSteeringTask().setSteeringIntensity(-3 * steeringValue);
 		car.steer(steeringValue);
 	}
+	/* */
 }
